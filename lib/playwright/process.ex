d58195a0c244887d53a7cba5d45f9ca4f209c566
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-11-15 23:15:05 +0700

    Start a driver process
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ lib/playwright/process.ex
@@ -0,0 +1,32 @@
+defmodule Playwright.Process do
+  @moduledoc """
+  Start a Playwright process with the given sub-command.
+  """
+
+  use GenServer
+
+  def start_link(args \\ []) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  @doc """
+  Stop driver process.
+  """
+  def stop(pid) do
+    GenServer.stop(pid)
+  end
+
+  @impl GenServer
+  def init(args) do
+    Process.flag(:trap_exit, true)
+
+    port =
+      Port.open(
+        {:spawn_executable, args[:playwright_bin_path]},
+        args: [args[:command]]
+      )
+
+    Port.monitor(port)
+    {:ok, %{process: port}}
+  end
+end
